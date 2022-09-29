cec2e0ca67c2f93c4200b27272bcaa72945884bc
Sai Chand <ghanta.sa@northeastern.edu>
2022-09-29 19:53:07 -0400

    added test case and readme file

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import com.example.demo.controller.MainController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    @Autowired
+    MainController controller;
+
     @Test
     void contextLoads() {
+        assertEquals(1+1, 2);
     }
 
 }
